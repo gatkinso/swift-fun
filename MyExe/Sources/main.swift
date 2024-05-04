ab3a18b0f43d09e5fc1401e6bbbf592f472a38a5
Geoffrey Atkinson <geoffrey_atkinso@hotmail.com>
2024-05-04 10:42:18 -0400

    Clean up, add a stupid closure mock.  Need to come up with something more advanced.

--- MyExe/Sources/main.swift
+++ MyExe/Sources/main.swift
@@ -4,16 +4,24 @@
 @_exported import MyLib
 
 func main() {
-   let z = myLibAdd(2, 3)
+    let z = myLibAdd(2, 3)
 
-   print("2+3 better equal: \(z)")
+    print("2+3 better equal: \(z)")
+
+    ///////////
 
     let provider = NetworkProvider()
     let controller = DataController(provider)
-
     let result = controller.doLookup()
 
     print("Look up returned: \(result)")
+
+    ///////////
+
+    let nm = NetworkManager()
+    let result1 = nm.someFunctionThatTakesAClosure(closure: theClosure)
+
+    print("Look up returned: \(result1)")
 }
 
 main()
